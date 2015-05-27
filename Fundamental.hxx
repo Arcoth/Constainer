aab9e9ee8eaec7595933eab1a5467e847580563c
Arcoth <columbo@gmx-topmail.de>
2015-05-27 11:27:45 +0100

    Added CHAR_BIT test to avoid problems at a later stage.

--- Fundamental.hxx
+++ Fundamental.hxx
@@ -1,8 +1,14 @@
 #ifndef FUNDAMENTAL_HXX_INCLUDED
 #define FUNDAMENTAL_HXX_INCLUDED
 
+#include <climits>
+
 #if __cplusplus < 201402L
-	#error "This library is intended for use with the C++14 programming language."
+	#error This library is intended for use with the C++14 programming language and following.
+#endif
+
+#if CHAR_BIT != 8
+	#error This library depends on char designating an octet. Your runtime environment is bad, and you should feel bad.
 #endif
 
 #ifndef CONSTAINER_PURE_CONST
