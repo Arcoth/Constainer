ba63377a0cd8ef694cc9a391f14e023761c185d2
Arcoth <columbo@gmx-topmail.de>
2015-08-09 13:20:41 +0200

    Added none_of, any_of, all_of, equal_range, binary_search, upper_bound and lower_bound. Added a NegateFunctor. Fixed a few bugs.

--- Algorithms.hxx
+++ Algorithms.hxx
@@ -12,6 +12,22 @@
 
 namespace Constainer {
 
+template <typename F>
+struct NegateFunctor {
+	F f;
+
+	constexpr NegateFunctor(F const& f) : f(f) {}
+
+	template <typename... Args>
+	constexpr bool operator()(Args&&... args) {
+		return !f(std::forward<Args>(args)...);
+	}
+};
+template <typename F>
+constexpr auto negateFunctor(F const& f) {
+	return NegateFunctor<F>(f);
+}
+
 namespace detail {
 	template <typename T>
 	constexpr void swap(T& a, T& b, ...) {
@@ -35,15 +51,21 @@ constexpr auto find(InputIterator first, InputIterator last, T const& val, Comp 
 		++first;
 	return first;
 }
-
 template <typename InputIterator, typename T>
 constexpr auto find(InputIterator first, InputIterator last, T const& val) {
 	return Constainer::find(first, last, val, std::equal_to<>());
 }
 
-template <typename InputIterator, typename T, typename Pred>
+template <typename InputIterator, typename Pred>
 constexpr auto find_if(InputIterator first, InputIterator last, Pred p) {
 	while (first != last && !p(*first))
+		++first;
+	return first;
+}
+
+template <typename InputIterator, typename Pred>
+constexpr auto find_if_not(InputIterator first, InputIterator last, Pred p) {
+	while (first != last && p(*first))
 		++first;
 	return first;
 }
@@ -55,7 +77,6 @@ constexpr auto copy(InputIt first, InputIt last, OutputIt out) {
 
 	return out;
 }
-
 template <typename InputIt, typename SizeType, typename OutputIt>
 constexpr auto copy_n(InputIt first, SizeType count, OutputIt out) {
 	while (count-- != 0)
@@ -71,17 +92,15 @@ constexpr auto copy_backward(BiDir  first, BiDir last, BiDir2 last2) {
 
 	return last2;
 }
-
 template <typename BiDir, typename BiDir2>
 constexpr auto move_backward(BiDir first, BiDir last, BiDir2 last2) {
 	return Constainer::copy_backward(make_move_iterator(first), make_move_iterator(last), last2);
 }
 
-template<class InputIt, class OutputIt>
+template <typename InputIt, typename OutputIt>
 constexpr auto move(InputIt first, InputIt last, OutputIt out) {
 	return Constainer::copy(make_move_iterator(first), make_move_iterator(last), out);
 }
-
 template <typename InputIt, typename SizeType, typename OutputIt>
 constexpr auto move_n(InputIt first, SizeType count, OutputIt out) {
 	return Constainer::copy_n(make_move_iterator(first), count, out);
@@ -94,7 +113,6 @@ constexpr auto fill(OutputIt first, OutputIt last, T const& value) {
 
 	return first;
 }
-
 template <typename OutputIt, typename SizeType, typename T>
 constexpr auto fill_n(OutputIt first, SizeType count, T const& value) {
 	for (SizeType i = 0; i++ < count;)
@@ -110,7 +128,6 @@ constexpr auto swap_ranges(ForwardIt first, ForwardIt last, ForwardIt2 first2) {
 
 	return first;
 }
-
 template <typename T, std::size_t N>
 constexpr void swap(T (&a)[N], T (&b)[N]) {
 	swap_ranges(a, a+N, b);
@@ -123,7 +140,6 @@ constexpr void iota(ForwardIterator first, ForwardIterator last, T value) {
 		++value;
 	}
 }
-
 template <typename ForwardIterator, typename SizeType, typename T>
 constexpr void iota_n(ForwardIterator first, SizeType n, T value) {
 	while (n--) {
@@ -140,7 +156,6 @@ constexpr T accumulate(InputIt first, InputIt last, T init, BinaryOp op)
 
 	return init;
 }
-
 template <typename InputIt, typename T>
 constexpr T accumulate(InputIt first, InputIt last, T init) {
 	Constainer::accumulate(first, last, init, std::plus<>());
@@ -181,7 +196,8 @@ constexpr std::pair<InputIt1, InputIt2> mismatch(InputIt1 first1, InputIt1 last1
 template <typename InputIt1, typename InputIt2, typename Compare>
 constexpr bool lexicographical_compare(InputIt1 first1, InputIt1 last1,
                                        InputIt2 first2, InputIt2 last2,
-                                       Compare comp) {
+                                       Compare comp)
+{
 	while (first1 != last1 && first2 != last2) {
 		if (comp(*first1, *first2)) return true;
 		if (comp(*first2, *first1)) return false;
@@ -196,16 +212,16 @@ constexpr bool lexicographical_compare(InputIt1 first1, InputIt1 last1,
 	return Constainer::lexicographical_compare(first1, last1, first2, last2, std::less<>());
 }
 
-template <typename Input1, typename Input2, typename Comp>
-constexpr bool equal( Input1 first1, Input1 last1, Input2 first2, Comp comp ) {
+template <typename Input1, typename Input2, typename Comparator>
+constexpr bool equal( Input1 first1, Input1 last1, Input2 first2, Comparator comp ) {
 	return Constainer::mismatch(first1, last1, first2, comp).first == last1;
 }
 template <typename Input1, typename Input2>
 constexpr bool equal( Input1 first1, Input1 last1, Input2 first2 ) {
 	return Constainer::equal(first1, last1, first2, std::equal_to<>());
 }
-template <typename Input1, typename Input2, typename Comp>
-constexpr bool equal( Input1 first1, Input1 last1, Input2 first2, Input2 last2, Comp comp ) {
+template <typename Input1, typename Input2, typename Comparator>
+constexpr bool equal( Input1 first1, Input1 last1, Input2 first2, Input2 last2, Comparator comp ) {
 	auto p = Constainer::mismatch(first1, last1, first2, last2, comp);
 	return p.first == last1 && p.second == last2;
 }
@@ -232,51 +248,113 @@ constexpr ForwardIt1 search(ForwardIt1 first, ForwardIt1 last,
 		}
 	}
 }
-
 template <typename ForwardIt1, typename ForwardIt2, typename BinaryPredicate>
 constexpr ForwardIt1 search(ForwardIt1 f, ForwardIt1 l,
                             ForwardIt2 sf, ForwardIt2 sl) {
 	return search(f, l, sf, sl, std::equal_to<>());
 }
 
-template <typename InputIt, typename ForwardIt, typename BinaryPredicate>
+template <typename InputIt, typename ForwardIt, typename Comparator>
 constexpr InputIt find_first_of(InputIt first, InputIt last,
-                                ForwardIt s_first, ForwardIt s_last,
-                                BinaryPredicate pred) {
+                                ForwardIt sfirst, ForwardIt slast,
+                                Comparator comp) {
 	for (; first != last; ++first)
-		for (auto it = s_first; it != s_last; ++it)
-			if (pred(*first, *it))
+		for (auto it = sfirst; it != slast; ++it)
+			if (comp(*first, *it))
 				return first;
 
 	return last;
 }
-template <typename InputIt, typename ForwardIt, typename BinaryPredicate>
-constexpr InputIt find_first_of(InputIt f, InputIt l,
-                                ForwardIt sf, ForwardIt sl) {
-	return find_first_of(f, l, sf, sl, std::equal_to<>());
+template <typename InputIt, typename ForwardIt>
+constexpr InputIt find_first_of(InputIt first, InputIt last,
+                                ForwardIt sfirst, ForwardIt slast) {
+	return find_first_of(first, last, sfirst, slast, std::equal_to<>());
 }
 
-
-template <typename InputIt, typename ForwardIt, typename BinaryPredicate>
+template <typename InputIt, typename ForwardIt, typename Comparator>
 constexpr InputIt find_first_not_of(InputIt first, InputIt last,
                                     ForwardIt s_first, ForwardIt s_last,
-                                    BinaryPredicate pred) {
+                                    Comparator comp) {
 	for (; first != last; ++first) {
 		auto it = s_first;
 		for (; it != s_last; ++it)
-			if (pred(*first, *it))
+			if (comp(*first, *it))
 				break;
 		if (it == s_last)
 			return first;
 	}
 	return last;
 }
-
 template <typename InputIt, typename ForwardIt>
 constexpr InputIt find_first_not_of(InputIt f, InputIt l,
                                     ForwardIt sf, ForwardIt sl) {
-	return find_first_not_of(f, l, sf, sl, std::equal_to<>());
+	return Constainer::find_first_not_of(f, l, sf, sl, std::equal_to<>());
 }
+
+template <typename ForwardIt, typename T, typename Comparator>
+constexpr ForwardIt lower_bound(ForwardIt first, ForwardIt last, T const& value, Comparator comp) {
+	auto count = Constainer::distance(first, last);
+	while (count > 0) {
+		auto intervall_size = count / 2;
+		auto it = Constainer::next(first, intervall_size);
+		if (comp(*it, value)) {
+			first = ++it;
+			count -= intervall_size + 1;
+		}
+		else
+			count = intervall_size;
+	}
+	return first;
+}
+template <typename ForwardIt, typename T>
+constexpr ForwardIt lower_bound(ForwardIt first, ForwardIt last, T const& value) {
+	return Constainer::lower_bound(first, last, value, std::less<>());
+}
+template<typename ForwardIt, typename T, typename Comparator>
+constexpr ForwardIt upper_bound(ForwardIt first, ForwardIt last, T const& value, Comparator comp) {
+	return Constainer::lower_bound(first, last, value, makeNegateFunctor(comp));
+}
+template<typename ForwardIt, typename T>
+constexpr ForwardIt upper_bound(ForwardIt first, ForwardIt last, T const& value) {
+	return Constainer::upper_bound(first, last, value, std::less<>());
+}
+
+template <typename ForwardIt, typename T, typename Comparator>
+constexpr ForwardIt binary_search(ForwardIt first, ForwardIt last, T const& value, Comparator comp) {
+	first = Constainer::lower_bound(first, last, value, comp);
+	return first != last && !comp(value, *first)? first : last;
+}
+template <typename ForwardIt, typename T>
+constexpr ForwardIt binary_search(ForwardIt first, ForwardIt last, T const& value) {
+	return Constainer::binary_search(first, last, value, std::less<>());
+}
+
+template<class ForwardIt, class T, class Compare>
+constexpr auto equal_range(ForwardIt first, ForwardIt last, const T& value, Compare comp) {
+	return std::make_pair(Constainer::lower_bound(first, last, value, comp),
+	                      Constainer::upper_bound(first, last, value, comp));
+}
+template<class ForwardIt, class T, class Compare>
+constexpr auto equal_range(ForwardIt first, ForwardIt last, const T& value) {
+	return Constainer::equal_range(first, last, value, std::less<>());
+}
+
+template < typename InputIt, typename UnaryPred>
+constexpr bool all_of(InputIt first, InputIt last, UnaryPred pred) {
+	return Constainer::find_if_not(first, last, pred) == last;
+}
+
+template < typename InputIt, typename UnaryPred>
+constexpr bool any_of(InputIt first, InputIt last, UnaryPred pred) {
+	return Constainer::find_if(first, last, pred) != last;
+}
+
+template <typename InputIt, typename UnaryPred>
+constexpr bool none_of(InputIt first, InputIt last, UnaryPred pred) {
+	return Constainer::find_if(first, last, pred) == last;
+}
+
+
 }
 
 #endif
