903aefaaea8dddc9da0b24852c621bcf7e05b2f7
Arcoth <columbo@gmx-topmail.de>
2015-08-09 13:21:51 +0200

    Introduced popcount and a trailing bit count. This is primarily intended for Bitset.

--- Math.hxx
+++ Math.hxx
@@ -25,7 +25,7 @@ namespace detail {
 
 template <typename T>
 CONSTAINER_PURE_CONST constexpr T signum(T x) {
-	return detail::signum(x, std::is_signed<T>());
+	return detail::signum(x, std::is_signed<T>{});
 }
 
 template <typename T>
@@ -83,6 +83,54 @@ pow(Float f, int e) {
 	}
 
 	return flip? 1/res : res;
+}
+
+
+/**< The day on which we look forward to using 512 bit integers is yet to come */
+CONSTAINER_PURE_CONST constexpr std::uint8_t popcount( std::uint64_t v ) {
+	#if defined __clang__ || defined __GNUG__
+		return __builtin_popcountll(v);
+	#else
+		// From http://graphics.stanford.edu/~seander/bithacks.html#CountBitsSetTable
+		/*static*/ const std::uint8_t table[256] =
+		{
+		#define B2(n) n,     n+1,     n+1,     n+2
+		#define B4(n) B2(n), B2(n+1), B2(n+1), B2(n+2)
+		#define B6(n) B4(n), B4(n+1), B4(n+1), B4(n+2)
+			B6(0), B6(1), B6(1), B6(2)
+		#undef B2
+		#undef B4
+		#undef B6
+		};
+
+		return table[ v           & 0xFF] +
+		       table[(v >> 8)     & 0xFF] +
+		       table[(v >> 16)    & 0xFF] +
+		       table[(v >> 24)    & 0xFF] +
+		       table[(v >> 32)    & 0xFF] +
+		       table[(v >> 40)    & 0xFF] +
+		       table[(v >> 48)    & 0xFF] +
+		       table[ v >> 56];
+	#endif
+}
+
+/**< The day on which we look forward to using 512 bit integers is yet to come */
+CONSTAINER_PURE_CONST constexpr std::uint8_t count_trailing( std::uint64_t v ) {
+	#if defined __clang__ || defined __GNUG__
+		return __builtin_ctzll(v);
+	#else
+		const int deBruijn[64] = {
+			 0,  1,  2, 53,  3,  7, 54, 27,
+			 4, 38, 41,  8, 34, 55, 48, 28,
+			62,  5, 39, 46, 44, 42, 22,  9,
+			24, 35, 59, 56, 49, 18, 29, 11,
+			63, 52,  6, 26, 37, 40, 33, 47,
+			61, 45, 43, 21, 23, 58, 17, 10,
+			51, 25, 36, 32, 60, 20, 57, 16,
+			50, 31, 19, 15, 30, 14, 13, 12,
+		};
+		return deBruijn[(v & -v) * 0x022FDD63CC95386D >> 27];
+	#endif
 }
 
 }
