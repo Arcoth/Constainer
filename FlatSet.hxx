0bc68c9e6b2300ef0b8d4965656791fcebaa4891
Arcoth <columbo@gmx-topmail.de>
2015-08-28 17:21:06 +0200

    Added Multiset.

--- FlatSet.hxx
+++ FlatSet.hxx
@@ -4,19 +4,26 @@
 
 #pragma once
 
-#include "UniqueFlatTree.hxx"
+#include "FlatUniqueTree.hxx"
+#include "FlatMultiTree.hxx"
 #include "StableVector.hxx"
 
 namespace Constainer {
 
 template <typename Key, typename Compare, typename Container>
-using BasicFlatSet = detail::UniqueFlatTree<Key, Key, IdentityFunctor, Compare, Container>;
-template <typename Key, typename Compare, typename Container>
-using BasicFlatSet = detail::UniqueFlatTree<Key, Key, IdentityFunctor, Compare, Container>;
+using BasicFlatSet = detail::FlatUniqueTree<Key, Key, IdentityFunctor, Compare, Container>;
 
 template <typename T, std::size_t MaxN, typename Compare = std::less<T>>
 using FlatSet = BasicFlatSet<T, Compare, Constainer::Vector<T, MaxN>>;
 template <typename T, std::size_t MaxN, typename Compare = std::less<T>>
-using StableFlatSet = BasicFlatSet<T, Compare, Constainer::StableVector<T, ChunkPool<T, MaxN>>>;
+using StableFlatSet = BasicFlatSet<T, Compare, Constainer::StableVector<T, MaxN>>;
+
+template <typename Key, typename Compare, typename Container>
+using BasicFlatMultiSet = detail::FlatMultiTree<Key, Key, IdentityFunctor, Compare, Container>;
+
+template <typename T, std::size_t MaxN, typename Compare = std::less<T>>
+using FlatMultiSet       = BasicFlatMultiSet<T, Compare, Constainer::Vector<T, MaxN>>;
+template <typename T, std::size_t MaxN, typename Compare = std::less<T>>
+using StableFlatMultiSet = BasicFlatMultiSet<T, Compare, Constainer::StableVector<T, MaxN>>;
 
 }
