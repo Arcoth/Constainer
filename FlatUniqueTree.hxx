b7d2bb4ce6df40785f9dd93ebfa5fc8912d9d362
Arcoth <columbo@gmx-topmail.de>
2015-08-28 17:28:43 +0200

    Removing the renamed file.

--- FlatUniqueTree.hxx
+++ FlatUniqueTree.hxx
@@ -10,7 +10,7 @@
 namespace Constainer { namespace detail {
 
 template <typename Key, typename Value, typename KeyOfValue, typename Compare, typename Container>
-class UniqueFlatTree
+class FlatUniqueTree
 	: public      FlatTree<Key, Value, KeyOfValue, Compare, Container> {
 	using _base = FlatTree<Key, Value, KeyOfValue, Compare, Container>;
 
@@ -20,44 +20,22 @@ class UniqueFlatTree
 	using _base::insert_unique;
 	using _base::insert_equal;
 	using _base::lower_bound_range;
-	// TODO: COMPLETE
+	using _base::emplace_equal;
+	using _base::emplace_hint_equal;
+	using _base::emplace_unique;
+	using _base::emplace_hint_unique;
+	using _base::equal_range;
 
 public:
 
+	// Convenience in this template definition
+	using typename _base::key_type;
+	using typename _base::value_type;
+	using typename _base::key_compare;
+	using typename _base::value_compare;
 	using typename _base::iterator;
 	using typename _base::const_iterator;
-	using typename _base::reverse_iterator;
-	using typename _base::const_reverse_iterator;
-
-	using typename _base::value_type;
-	using typename _base::key_type;
-	using typename _base::value_compare;
-	using typename _base::key_compare;
-	using typename _base::pointer;
-	using typename _base::reference;
-	using typename _base::const_pointer;
 	using typename _base::const_reference;
-	using typename _base::size_type;
-
-	using _base::key_comp;
-	using _base::value_comp;
-	using _base::clear;
-	using _base::erase;
-	using _base::empty;
-	using _base::size;
-	using _base::max_size;
-
-	using _base::begin;
-	using _base::end;
-	using _base::cbegin;
-	using _base::cend;
-	using _base::crbegin;
-	using _base::crend;
-
-	using _base::lower_bound;
-	using _base::upper_bound;
-	using _base::find;
-	using _base::count;
 
 	constexpr       _iter_pair equal_range(key_type const& key)       {return lower_bound_range(key);}
 	constexpr _const_iter_pair equal_range(key_type const& key) const {return lower_bound_range(key);}
@@ -66,35 +44,35 @@ public:
 	template <typename T>
 	constexpr has_is_transparent<key_compare, _const_iter_pair> equal_range(T const& t) const {return lower_bound_range(t);}
 
-	constexpr UniqueFlatTree() : _base{} {}
-	constexpr UniqueFlatTree(UniqueFlatTree const&) = default;
-	constexpr UniqueFlatTree(UniqueFlatTree &&) = default;
-	constexpr UniqueFlatTree& operator=(UniqueFlatTree const&) = default;
-	constexpr UniqueFlatTree& operator=(UniqueFlatTree &&) = default;
+	constexpr FlatUniqueTree() : _base{} {}
+	constexpr FlatUniqueTree(FlatUniqueTree const&) = default;
+	constexpr FlatUniqueTree(FlatUniqueTree &&) = default;
+	constexpr FlatUniqueTree& operator=(FlatUniqueTree const&) = default;
+	constexpr FlatUniqueTree& operator=(FlatUniqueTree &&) = default;
 
-	explicit UniqueFlatTree( value_compare const& comp ) : _base(comp) {}
+	explicit FlatUniqueTree( value_compare const& comp ) : _base(comp) {}
 
 	template <typename InputIt>
-	constexpr UniqueFlatTree(InputIt first, InputIt last, value_compare const& comp) :
+	constexpr FlatUniqueTree(InputIt first, InputIt last, value_compare const& comp) :
 		_base(std::true_type{}, first, last, comp) {}
 	template <typename InputIt>
-	constexpr UniqueFlatTree(InputIt first, InputIt last) :
-		UniqueFlatTree(first, last, value_compare{}) {}
+	constexpr FlatUniqueTree(InputIt first, InputIt last) :
+		FlatUniqueTree(first, last, value_compare{}) {}
 	template <typename InputIt>
-	constexpr UniqueFlatTree(ordered_unique_range_t, InputIt first, InputIt last, value_compare const& comp) :
+	constexpr FlatUniqueTree(ordered_unique_range_t, InputIt first, InputIt last, value_compare const& comp) :
 		_base(ordered_range, first, last, comp) {}
 	template <typename InputIt>
-	constexpr UniqueFlatTree(ordered_unique_range_t, InputIt first, InputIt last) :
-		UniqueFlatTree(ordered_unique_range, first, last, value_compare{}) {}
+	constexpr FlatUniqueTree(ordered_unique_range_t, InputIt first, InputIt last) :
+		FlatUniqueTree(ordered_unique_range, first, last, value_compare{}) {}
 
-	constexpr UniqueFlatTree(std::initializer_list<value_type> ilist) :
-		UniqueFlatTree(ilist.begin(), ilist.end()) {}
-	constexpr UniqueFlatTree(ordered_unique_range_t, std::initializer_list<value_type> ilist) :
-		UniqueFlatTree(ordered_unique_range, ilist.begin(), ilist.end()) {}
-	constexpr UniqueFlatTree(std::initializer_list<value_type> ilist, value_compare const& vcmp) :
-		UniqueFlatTree(ilist.begin(), ilist.end(), vcmp) {}
-	constexpr UniqueFlatTree(ordered_unique_range_t, std::initializer_list<value_type> ilist, value_compare const& vcmp) :
-		UniqueFlatTree(ordered_unique_range, ilist.begin(), ilist.end(), vcmp) {}
+	constexpr FlatUniqueTree(std::initializer_list<value_type> ilist) :
+		FlatUniqueTree(ilist.begin(), ilist.end()) {}
+	constexpr FlatUniqueTree(ordered_unique_range_t, std::initializer_list<value_type> ilist) :
+		FlatUniqueTree(ordered_unique_range, ilist.begin(), ilist.end()) {}
+	constexpr FlatUniqueTree(std::initializer_list<value_type> ilist, value_compare const& vcmp) :
+		FlatUniqueTree(ilist.begin(), ilist.end(), vcmp) {}
+	constexpr FlatUniqueTree(ordered_unique_range_t, std::initializer_list<value_type> ilist, value_compare const& vcmp) :
+		FlatUniqueTree(ordered_unique_range, ilist.begin(), ilist.end(), vcmp) {}
 
 	constexpr std::pair<iterator, bool> insert(const_reference val) {
 		return _base::insert_unique(val);
