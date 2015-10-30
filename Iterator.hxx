7d2d03723f159ebf2583812a0109c6a22f6386a3
Arcoth <columbo@gmx-topmail.de>
2015-10-30 18:43:46 +0000

    Added transform_iterator, + style changes.

--- Iterator.hxx
+++ Iterator.hxx
@@ -7,6 +7,7 @@
 #include "impl/IteratorTraits.hxx"
 
 #include <iterator>
+#include <tuple>
 
 namespace Constainer {
 
@@ -168,7 +169,7 @@ constexpr auto operator- (const reverse_iterator<I>& lhs, const reverse_iterator
 { return rhs.base() - lhs.base(); }
 
 template<typename I>
-constexpr reverse_iterator<I>
+constexpr auto
 operator+(typename reverse_iterator<I>::difference_type n, const reverse_iterator<I>& lhs)
 { return reverse_iterator<I>(lhs.base() - n); }
 
@@ -202,8 +203,7 @@ constexpr decltype(auto) operator-(const reverse_iterator<I1>& lhs, const revers
 { return rhs.base() - lhs.base(); }
 
 template <typename I>
-constexpr reverse_iterator<I> make_reverse_iterator( I i )
-{ return reverse_iterator<I>(i); }
+constexpr auto make_reverse_iterator( I i ) {return reverse_iterator<I>(i);}
 
 /// MOVE ITERATOR:
 
@@ -294,9 +294,151 @@ operator+(typename move_iterator<I>::difference_type n, move_iterator<I> const& 
 { return lhs + n; }
 
 template <typename I>
-constexpr move_iterator<I> make_move_iterator(I i)
-{ return move_iterator<I>(i); }
+constexpr auto make_move_iterator(I i) {return move_iterator<I>(i);}
 
+/// TRANSFORM ITERATOR
+
+template <typename Extractor, typename Iterator>
+struct transform_iterator
+{
+	using iterator_type = Iterator;
+	using extractor_type = Extractor;
+
+	using reference = std::result_of_t<
+	                    extractor_type const&(typename std::iterator_traits<iterator_type>::reference)
+	                  >;
+	using value_type = std::decay_t<reference>;
+	using pointer = value_type*;
+	using difference_type = typename std::iterator_traits<Iterator>::difference_type;
+	using iterator_category = typename std::iterator_traits<Iterator>::iterator_category;
+
+private:
+
+	std::tuple<Extractor, iterator_type> _data;
+	constexpr decltype(auto) _it()       {return std::get<iterator_type>(_data);}
+	constexpr decltype(auto) _ex() const {return std::get<extractor_type>(_data);}
+
+public:
+
+	constexpr auto extractor() const {return _ex();}
+	constexpr auto base() const {return std::get<iterator_type>(_data);}
+
+	constexpr explicit transform_iterator(iterator_type it, extractor_type const& ex = {}) : _data(ex, it) {}
+
+	template <typename Q, typename Iter2>
+	constexpr transform_iterator(transform_iterator<Q, Iter2> const& other)
+		: _data(other._data) {}
+
+	constexpr reference operator*() const {return _ex()(*base());}
+
+	constexpr pointer operator->() const {return &operator*();}
+
+	constexpr transform_iterator& operator++() {
+		++_it();
+		return *this;
+	}
+
+	constexpr transform_iterator operator++(int) {
+		auto t = *this;
+		++_it();
+		return t;
+	}
+
+	constexpr transform_iterator& operator--() {
+		--_it();
+		return *this;
+	}
+
+	constexpr transform_iterator operator--(int) {
+		transform_iterator t = *this;
+		--_it();
+		return t;
+	}
+
+	constexpr transform_iterator operator+(difference_type n) const
+	{ return transform_iterator(base() + n); }
+
+	constexpr transform_iterator& operator+=(difference_type n) {
+		_it() += n;
+		return *this;
+	}
+
+	constexpr transform_iterator operator-(difference_type n) const
+	{ return transform_iterator(base() - n); }
+
+	constexpr transform_iterator& operator-=(difference_type n) {
+		_it() -= n;
+		return *this;
+	}
+
+	constexpr reference operator[](difference_type n) const
+	{ return *(*this + n); }
+};
+
+template <typename P, typename I>
+constexpr bool operator==(transform_iterator<P,I> const& lhs, transform_iterator<P,I> const& rhs)
+{ return lhs.base() == rhs.base(); }
+
+template <typename P, typename I>
+constexpr bool operator <(transform_iterator<P,I> const& lhs, transform_iterator<P,I> const& rhs)
+{ return rhs.base() < lhs.base(); }
+
+template<typename P, typename I>
+constexpr bool operator!=(transform_iterator<P,I> const& lhs, transform_iterator<P,I> const& rhs)
+{ return !(lhs == rhs); }
+
+template <typename P, typename I>
+constexpr bool operator >(transform_iterator<P,I> const& lhs, transform_iterator<P,I> const& rhs)
+{ return rhs < lhs; }
+
+template <typename P, typename I>
+constexpr bool operator<=(transform_iterator<P,I> const& lhs, transform_iterator<P,I> const& rhs)
+{ return !(rhs < lhs); }
+
+template <typename P, typename I>
+constexpr bool operator>=(transform_iterator<P,I> const& lhs, transform_iterator<P,I> const& rhs)
+{ return !(lhs < rhs); }
+
+template <typename P, typename I>
+constexpr auto operator-(transform_iterator<P,I> const& lhs, transform_iterator<P,I> const& rhs)
+{ return rhs.base() - lhs.base(); }
+
+template <typename P, typename I>
+constexpr auto operator+(typename transform_iterator<P,I>::difference_type n, transform_iterator<P,I> const& lhs)
+{ return transform_iterator<P,I>(lhs.base() - n); }
+
+template <typename P1, typename I1, typename P2, typename I2>
+constexpr bool operator==(transform_iterator<P1,I1> const& lhs, transform_iterator<P2,I2> const& rhs)
+{ return lhs.base() == rhs.base(); }
+
+template <typename P1, typename I1, typename P2, typename I2>
+constexpr bool operator <(transform_iterator<P1,I1> const& lhs, transform_iterator<P2,I2> const& rhs)
+{ return rhs.base() < lhs.base(); }
+
+template <typename P1, typename I1, typename P2, typename I2>
+constexpr bool operator!=(transform_iterator<P1,I1> const& lhs, transform_iterator<P2,I2> const& rhs)
+{ return !(lhs == rhs); }
+
+template <typename P1, typename I1, typename P2, typename I2>
+constexpr bool operator >(transform_iterator<P1,I1> const& lhs, transform_iterator<P2,I2> const& rhs)
+{ return rhs < lhs; }
+
+template <typename P1, typename I1, typename P2, typename I2>
+constexpr bool operator<=(transform_iterator<P1,I1> const& lhs, transform_iterator<P2,I2> const& rhs)
+{ return !(rhs < lhs); }
+
+template <typename P1, typename I1, typename P2, typename I2>
+constexpr bool operator>=(transform_iterator<P1,I1> const& lhs, transform_iterator<P2,I2> const& rhs)
+{ return !(lhs < rhs); }
+
+template <typename P1, typename I1, typename P2, typename I2>
+constexpr decltype(auto) operator-(transform_iterator<P1,I1> const& lhs, transform_iterator<P2,I2> const& rhs)
+{ return rhs.base() - lhs.base(); }
+
+template <typename E, typename I>
+constexpr auto make_transform_iterator(I i, E const& e) {
+	return transform_iterator<E,I>(i,e);
 }
 
-#include "RangeAccess.hxx"
+
+}
